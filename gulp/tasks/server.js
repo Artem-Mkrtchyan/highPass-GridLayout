6847c2ec23695829b8a1045d5e85ceb5fe6ed996
Artem Mkrtchyan <mkr2263@yandex.ru>
2022-10-16 13:18:03 +0300

    delete files

--- gulp/tasks/server.js
+++
@@ -1,9 +0,0 @@
-export const server = (done) => {
-  app.plugins.browsersync.init({
-    server: {
-      baseDir: `${app.path.build.html}` //базовая папка из которой запускаются файлы
-    },
-    notify: false,
-    port: 3000,
-  });
-}
